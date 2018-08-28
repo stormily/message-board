f415145bf4dd5b94adf45a117549d97ed1e53733
stormily <773009690@qq.com>
2018-08-28 22:13:31 +0800

    sqlite3

--- python app.py
+++ python app.py
@@ -1,6 +1,20 @@
 from flask import Flask, request, render_template
+import datetime
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+cursor = conn.cursor()
+try:
+    cursor.execute('create table message (id integar primary key, username varchar(64), message text)')
+except Exception as e:
+    print('数据表已存在')
 
 app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def sign():
+    return render_template('board.html')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,15 +28,18 @@ def hello_world():
         with open('message.txt', 'a') as f:
             f.write(message + '\n')
             print(message)
-    return render_template('board.html')
-
-@app.route('/', methods = ['POST'])
-def signin():
     username = request.form['username']
     message = request.form['message']
-    return render_template('signin-ok.html', username = username)
+    #check_same_thread这个设置为False，即可允许sqlite被多个线程同时访问 （未修改实现）
+    if(username and message):
+        cursor.execute('insert into message (username, message) values (?, ?)', [username, message])
+        conn.commit()
+
+    return render_template('sign.html',username=username, message=message)
+
+#应该还有一个实现把数据库链接网页输出的函数，也没能实现
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
